7ed654189e7fac4daf39275c0d0623e6ade3c668
Ghali418 <rqht0564@leeds.ac.uk>
2026-03-27 12:35:53 +0000

    Update salary.c

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -1,23 +1,41 @@
 
 #include <stdio.h>
+#include <string.h>
 
-/*
- * Potrfolio submission
- * Name:
- * ID:
- */
+int main() {
+  float salary = 36250.0;
+  float ni_rate = 8.0;
+  float tax_rate = 15.0;
 
- int main( void ) {
+  // variables to hold contributions
+  float ni_contribution;
+  float tax_contribution;
+  float salary_after_ni;
+  float take_home;
 
-    // define and initialise variables for the problem data 
+  // step 1 : Calcualte nation insurance contribution
+  ni_contribution = salary * (ni_rate / 100.0);
 
-    // calculate the deductions and final take-home salary
+  // step 2 : calculate salary after NI deduction
+  salary_after_ni = salary - ni_contribution;
 
-    // Use only these print statement with appropriate formatting and variable names
-    //printf("Salary £...",var_name);
-    //printf("NI contribution £...",var_name);
-    //printf("Tax contribution £...",var_name);
-    //printf("Take home salary £...",var_name);
+  // step 3 - Calcualte taxable amoutn (only above 12500)
+  float taxable_amount = salary_after_ni - 12500.0;
 
-    return 0;
- }
+  if (taxable_amount < 0) {
+    taxable_amount = 0; // no tax if under 12500
+  }
+
+  // step 4 : calculate tax contribution
+  tax_contribution = taxable_amount * (tax_rate / 100.0);
+
+  // step 5 : calculate take home salary
+  take_home = salary_after_ni - tax_contribution;
+
+  // step 6 : Print results with 2 decimal places
+  printf("National Insurance contribution : %.2f\n", ni_contribution);
+  printf("Tax contribution : %.2f\n", tax_contribution);
+  printf("Take home salary : %.2f\n", take_home);
+
+  return 0;
+}
